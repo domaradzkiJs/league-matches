99fa32668bab37ac8c2581b5b254539d91fd5ddd
domaradzkiJs <56085891+domaradzkiJs@users.noreply.github.com>
2019-11-28 19:17:53 +0100

    first commit, testing api

---
+++ src/lolApi.data.js
@@ -0,0 +1,5 @@
+
+const myProxy = 'https://guarded-plateau-90176.herokuapp.com/';
+
+const API_KEY = "RGAPI-8f9065ef-c634-438e-a398-1a2375056de5";
+export const Summoner = `${myProxy}https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/WilkFenris?api_key=${API_KEY}`;
